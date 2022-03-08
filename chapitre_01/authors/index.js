cfd80f57032f89678f6cfcc5fa662d8a0843101e
anitamayousse <anita.mayousse@gmail.com>
2022-03-08 13:35:46 +0100

    revision

--- chapitre_01/authors/index.js
+++ chapitre_01/authors/index.js
@@ -1,108 +1,73 @@
 const express = require("express");
 const app = express();
 
-var authors = [
-    {
-        name: "Lawrence Nowell",
-        nationality: "UK",
-        books: ["Beowulf"]
-    },
-    {
-        name: "William Shakespeare",
-        nationality: "UK",
-        books: ["Hamlet", "Othello", "Romeo and Juliet", "MacBeth"]
-    },
-    {
-        name: "Charles Dickens",
-        nationality: "US",
-        books: ["Oliver Twist", "A Christmas Carol"]
-    },
-    {
-        name: "Oscar Wilde",
-        nationality: "UK",
-        books: ["The Picture of Dorian Gray", "The Importance of Being Earnest"]
-    },
-]
+const authors = [
+	{
+		name: "Lawrence Nowell",
+		nationality: "UK",
+		books: ["Beowulf"],
+	},
+	{
+		name: "William Shakespeare",
+		nationality: "UK",
+		books: ["Hamlet", "Othello", "Romeo and Juliet", "MacBeth"],
+	},
+	{
+		name: "Charles Dickens",
+		nationality: "US",
+		books: ["Oliver Twist", "A Christmas Carol"],
+	},
+	{
+		name: "Oscar Wilde",
+		nationality: "UK",
+		books: [
+			"The Picture of Dorian Gray",
+			"The Importance of Being Earnest",
+		],
+	},
+];
 
-//ex-01
-app.get("/", (req, res) => {
+// Routes
+app.get("/", (_req, res) => {
 	res.send("Authors API");
 });
-//ex-02
-app.get("/authors/1/", (req, res) => {
-	res.send("Lawrence Nowell, UK");
+
+app.get("/authors/:id", (req, res) => {
+	// const author = authors.find((x, index) => {
+	// 	const id = index + 1;
+
+	// 	return req.params.id === id.toString();
+	// });
+
+	const author = authors[req.params.id - 1];
+
+	res.send(`${author.name}, ${author.nationality}`);
 });
 
-app.get("/authors/2/", (req, res) => {
-	res.send("William Shakespeare, UK");
+app.get("/authors/:id/books", (req, res) => {
+	const books = authors[req.params.id - 1].books;
+	res.send(books.join(", "));
 });
 
-app.get("/authors/3/", (req, res) => {
-	res.send("Charles Dickens, US");
+app.get("/json/authors/:id", (req, res) => {
+	const author = authors[req.params.id];
+	delete author.books;
+	res.json(author);
 });
 
-app.get("/authors/4/", (req, res) => {
-	res.send("Oscar Wilde, UK");
-});
-//ex-03
-app.get("/authors/1/books/", (req, res) => {
-	res.send("Beowulf");
-});
-
-app.get("/authors/2/books/", (req, res) => {
-	res.send("/authors/2/books/");
+app.get("/json/authors/:id/books", (req, res) => {
+	const books = authors[req.params.id].books;
+	res.json({
+		books,
+	});
 });
 
-app.get("/authors/3/books/", (req, res) => {
-	res.send("Oliver Twist, A Christmas Carol");
+// Handle errors
+app.get("*", (_req, res) => {
+	res.status(404).send("Page not found");
 });
 
-app.get("/authors/4/books/", (req, res) => {
-	res.send("Oliver Twist, A Christmas Carol");
-});
-//ex-04
-
-
-app.get('/json/authors/:id', (req, res) => {
-
-    const author = authors[parseInt(req.params.id)];
-
-    if (!author) {
-        return res.json({message :
-             "Book not found!",})
-    }
-
-    res.json({
-        books : author.books
-    });
-})
-
-
-app.get('/json/authors/:id/books', (req, res) => {
-    
-
-    const author = authors[parseInt(req.params.id)];
-
-    if (!author) {
-        return res.json({message : 
-            "Book not found",})
-    }
-
-    res.json({
-        books : author.books
-    });
-})
-
-
-app.get('*', (req,res) => {
-
-    res.json({message : '404 NOT FOUND'})
-})
-
-app.get("*", (req, res) => {
-	res.send("Page not found - 404");
-});
-
+// Start the server
 app.listen(8000, () => {
-	console.log("Listening on port 8000"); // localhost:8000
+	console.log("Listening");
 });

01298bbca0c6e3ab5db821f42a7d8ab28986624b
anitamayousse <anita.mayousse@gmail.com>
2022-03-21 15:30:09 +0100

    ignore config

--- chapitre_01/authors/index.js
+++ chapitre_01/authors/index.js
@@ -1,38 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv")
 dotenv.config({
-	path: "./config.env",
+	path: "/config.env",
 });
 const { Pool } = require("pg");
 console.log(Pool);
 const app = express();
 
 const Postgres = new Pool ({ ssl: { rejectUnauthorized: false }});
-const authors = [
-	{
-		name: "Lawrence Nowell",
-		nationality: "UK",
-		books: ["Beowulf"],
-	},
-	{
-		name: "William Shakespeare",
-		nationality: "UK",
-		books: ["Hamlet", "Othello", "Romeo and Juliet", "MacBeth"],
-	},
-	{
-		name: "Charles Dickens",
-		nationality: "US",
-		books: ["Oliver Twist", "A Christmas Carol"],
-	},
-	{
-		name: "Oscar Wilde",
-		nationality: "UK",
-		books: [
-			"The Picture of Dorian Gray",
-			"The Importance of Being Earnest",
-		],
-	},
-];
 
 // Routes
 app.get("/",async (_req, res) => {

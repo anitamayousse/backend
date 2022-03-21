09ccb1998ab3f951123f6225822760839205623c
anitamayousse <anita.mayousse@gmail.com>
2022-03-21 16:26:29 +0100

    SQL done

--- chapitre_01/authors/index.js
+++ chapitre_01/authors/index.js
@@ -1,32 +1,49 @@
 const express = require("express");
 const dotenv = require("dotenv")
 dotenv.config({
-	path: "/config.env",
+	path: "./config.env",
 });
 const { Pool } = require("pg");
+const { bool } = require("joi");
 console.log(Pool);
 const app = express();
 
 const Postgres = new Pool ({ ssl: { rejectUnauthorized: false }});
 
 // Routes
+
+//with SQL
 app.get("/",async (_req, res) => {
 	const authors = await Postgres.query ("SELECT * FROM authors")
 		console.log(res);
 
 	res.json(authors.rows);
 });
-
-app.get("/authors/:id", (req, res) => {
-	// const author = authors.find((x, index) => {
-	// 	const id = index + 1;
-
-	// 	return req.params.id === id.toString();
-	// });
-
-	const author = authors[req.params.id - 1];
-
-	res.send(`${author.name}, ${author.nationality}`);
+//with SQL
+app.get("/authors/:id", async (req, res) => {
+	let authors;
+	try {
+		authors = await Postgres.query("SELECT * FROM authors WHERE authors.id=$1", [req.params.id]);
+	} catch (err) {
+		console.error(err);
+		return res.status(400).json({
+			message: "Erorr: Please enter a number",
+		});
+	}
+	res.json(authors.rows);
+});
+//with SQL
+app.get("/authors/:id/books", async (req, res) => {
+	let books;
+	try {
+		books = await Postgres.query("SELECT books FROM authors WHERE authors.id=$1", [req.params.id]);
+	} catch (err) {
+		console.error(err);
+		return res.status(400).json({
+			message: "Erorr: Please enter a number",
+		});
+	}
+	res.json(books.rows);
 });
 
 app.get("/authors/:id/books", (req, res) => {
